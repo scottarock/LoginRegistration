65578c7c2ea8e184d185195d368685be81dbc835
Scott Rock <scottrockbiz@gmail.com>
2018-09-03 10:47:49 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express'),
+      parser = require('body-parser'),
+      session = require('express-session'),
+      path = require('path'),
+      port = process.env.PORT || 8000,
+      app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('client', 'views'));
+app.use(express.static(path.resolve('client', 'static')));
+app.use(parser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'lkauhjflksa',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 },
+}));
+
+app.listen(port, () => console.log(`app running on port ${port}`));
